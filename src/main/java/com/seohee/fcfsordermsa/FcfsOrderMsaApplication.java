29bb219d838c1ee3d97e701e4b10619cfff32641
seohee-P <p03061180@gmail.com>
2024-08-27 19:24:33 +0900

    Initial commit

---
+++ src/main/java/com/seohee/fcfsordermsa/FcfsOrderMsaApplication.java
@@ -0,0 +1,13 @@
+package com.seohee.fcfsordermsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcfsOrderMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcfsOrderMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seohee/fcfsordermsa/FcfsOrderMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seohee.fcfsordermsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcfsOrderMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
