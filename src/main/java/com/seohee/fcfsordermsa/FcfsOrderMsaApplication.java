ab738c327bb8a1fbacb6eae575294b85d35ca739
seohee-P <p03061180@gmail.com>
2024-08-28 02:00:37 +0900

    feat: JpaAuditing을 사용해 createdAt, updatedAt 저장

--- src/main/java/com/seohee/fcfsordermsa/FcfsOrderMsaApplication.java
+++ src/main/java/com/seohee/fcfsordermsa/FcfsOrderMsaApplication.java
@@ -1,8 +1,10 @@
 package com.seohee.fcfsordermsa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FcfsOrderMsaApplication {
 
